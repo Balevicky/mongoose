67ff72c8a40f3312728b5ab1cb79cd166b253574
Balevicky <balevictorien@yahoo.fr>
2025-04-06 10:54:33 +0000

    modified index.js

--- index.js
+++ index.js
@@ -17,12 +17,33 @@ async function main() {
   // await user1.save();
   // =================== ou =================
   // creer et enregister user dans mongodb
-  const user2 = await User.create({
-    email: "alice@yahoo.fr",
-    firstname: "Alice ",
-    age: 25,
-  });
-  console.log(user2);
+  // const user2 = await User.create({
+  //   email: "alice@yahoo.fr",
+  //   firstname: "Alice ",
+  //   age: 25,
+  // });
+  // console.log(user2);
+  // ============================Afficher tous les user
+  // const users = await User.find();
+  // console.log(users);
+  // ============================Afficher un user par sonID
+  // const Alice = await User.findById("67f18a30160170a06064a218");
+  // console.log(Alice);
+  // ============================Afficher un user par son nom
+
+  // const Alice = await User.find({
+  //   // firstname: "Alice ",
+  //   // age: { $gte: 27 },//>=27
+  //   age: { $lte: 27 }, //<=27
+  // });
+  // ============================Afficher des users par plusieur condition
+  // firstname: "Alice ";
+  const Alice = await User.find({
+    $or: [{ firstname: "Alice " }, { age: { $gte: 30 } }],
+  })
+    .select("firstname age -_id") // permet d'afficher uniquement firstname et age et exclure id
+    .sort({ firstname: "asc" }); // filtrer par ordre croissant soit par asc ou 1 ou  firstname
+  console.log(Alice);
 
   // =============== deconnecter mongoose==========
   mongoose.disconnect();
